429866ea84f38bd49e192e57871ed8aa101fcfc0
leon <554638047@qq.com>
2025-03-14 17:54:27 +0800

    feat: test api

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -2,13 +2,15 @@ export async function POST(request: Request) {
   // Parse the request body
   const body = await request.json();
 
-  const result = await fetch(`${process.env.API_URL}/login`, {
+  const response = await fetch(`${process.env.API_URL}/login`, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
     },
   });
+
+  const result = await response.json();
 
   return new Response(JSON.stringify(result), {
     status: 201,
