160f5580e0c4cf2601d56d6d3125da5cb9212499
leon <554638047@qq.com>
2025-03-17 15:18:50 +0800

    feat: try catch

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,15 +1,19 @@
-import axios from "axios";
-
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
-  const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-    username,
-    password
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' }
   })
+  // Transform or forward the response 
+  const data = await response.json();
 
   const transformed = { 
-    ...response, 
+    ...data, 
     params : {
       body
     }
