303879ff9777f807834920ed957a28f3483f5fea
leon <554638047@qq.com>
2025-03-17 16:07:31 +0800

    feat: try catch

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,32 +1,50 @@
+
+type User = {
+  username: string;
+  password: string;
+}
+
+function resolveAfter2Seconds(args: User) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        args
+      });
+    }, 2000);
+  });
+}
+
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
 
-  const response = await fetch(`${process.env.API_URL}/login`, {
-    method: 'POST',
-    body: JSON.stringify({
-      username,
-      password
-    }),
-    headers: { 'Content-Type': 'application/json' }
-  })
+  // const response = await fetch(`${process.env.API_URL}/login`, {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     username,
+  //     password
+  //   }),
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
+  const data = await resolveAfter2Seconds({
+    username, 
+    password
+  }) as User;
   // Transform or forward the response 
-  const data = await response.json();
+  // const data = await response.json();
 
 
   const transformed = { 
     ...data, 
     params : {
       body
-    }
+    },
+    id: 1
   };
  
   return new Response(JSON.stringify(transformed), {
     headers: { 
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Content-Type': 'application/json'
      },
   });
 }
