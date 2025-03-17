026d16c09b4ae1a41bea52378bda2ba9dd0b78b8
leon <554638047@qq.com>
2025-03-17 13:28:02 +0800

    feat: loginh

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,19 +1,28 @@
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     username,
+  //     password,
+  //     userId: 1,
+  //   }),
+  //   headers: {
+  //     'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  // `${process.env.API_URL}/login`
+  const response = await fetch('/login', {
     method: 'POST',
     body: JSON.stringify({
       username,
-      password,
-      userId: 1,
+      password
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-  // `${process.env.API_URL}/login`
-
   
   // Transform or forward the response 
   const data = await response.json();
