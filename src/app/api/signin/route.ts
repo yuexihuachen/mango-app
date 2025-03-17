ee13cc5030fbfa4c6e22af34004ec25eb7248f97
leon <554638047@qq.com>
2025-03-17 14:08:52 +0800

    feat: try catch

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,19 +1,7 @@
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
-  // const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     username,
-  //     password,
-  //     userId: 1,
-  //   }),
-  //   headers: {
-  //     'Content-type': 'application/json; charset=UTF-8',
-  //   },
-  // })
-  // `${process.env.API_URL}/login`
-  const response = await fetch(`${process.env.API_URL}/login`, {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
       username,
@@ -21,6 +9,15 @@ export async function POST(request: Request) {
     }),
     headers: { 'Content-Type': 'application/json' }
   })
+
+  // const response = await fetch(`${process.env.API_URL}/login`, {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     username,
+  //     password
+  //   }),
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
   // Transform or forward the response 
   const data = await response.json();
 
