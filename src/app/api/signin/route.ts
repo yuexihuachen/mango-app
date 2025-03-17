526c8209442238b4052fd41051a9c287d69c6c3e
leon <554638047@qq.com>
2025-03-17 10:29:17 +0800

    feat: test jsonplaceholder post method params

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,11 +1,11 @@
 export async function POST(request: Request) {
   const body = await request.json();
-  const { username, passwrod } = body;
+  const { username, password } = body;
   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
       username,
-      passwrod,
+      password,
       userId: 1,
     }),
     headers: {
