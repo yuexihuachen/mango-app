b48ddcbd4bd0d8a76c42a88a9e6362b22878d0cd
leon <554638047@qq.com>
2025-03-17 13:54:46 +0800

    feat: try catch

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -13,35 +13,23 @@ export async function POST(request: Request) {
   //   },
   // })
   // `${process.env.API_URL}/login`
-  let data, error, otherdata;
-  try {
-    const response = await fetch(`${process.env.API_URL}/login`, {
-      method: 'POST',
-      body: JSON.stringify({
-        username,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(res => {
-      return res.json()
-    }).catch(err => {
-      error = err
-    })
-    otherdata = response
-    // Transform or forward the response 
-    data = await response.json();
-  } catch(err) {
-    error = err
-  }
+  const response = await fetch(`${process.env.API_URL}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  // Transform or forward the response 
+  const data = await response.json();
 
 
   const transformed = { 
     ...data, 
     params : {
       body
-    },
-    error,
-    otherdata
+    }
   };
  
   return new Response(JSON.stringify(transformed), {
