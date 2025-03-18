01bdcfe62f0af153b2ef63d3576174f346bd7d89
leon <554638047@qq.com>
2025-03-18 16:48:04 +0800

    feat: config

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -2,12 +2,13 @@ export async function POST(req: Request) {
   // Parse the request body
   const body = await req.json();
 
-  const request = new Request('https://jsonplaceholder.typicode.com/posts', {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify(body),
+    headers: {
+        "Content-Type": "application/json"
+      }
   });
-
-  const res = await fetch(request);
 
   return Response.json({ 
     response: res,
