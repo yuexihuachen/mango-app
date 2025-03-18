ef79dcdc9e727dde64a957de0c7876597bf316ee
leon <554638047@qq.com>
2025-03-18 13:17:34 +0800

    feat: try catch

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,44 +1,17 @@
-import axios from 'axios';
-
-type User = {
-  username: string;
-  password: string;
-}
-
-function resolveAfter2Seconds(args: User) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      axios.post(`${process.env.API_URL}/login`, {
-        ...args
-      }).then((res) => {
-        resolve(res)
-      }).catch(err => {
-        resolve(err)
-      })
-    }, 0);
-  });
-}
-
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, password } = body;
 
-  // const response = await fetch(`${process.env.API_URL}/login`, {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     username,
-  //     password
-  //   }),
-  //   headers: { 'Content-Type': 'application/json' }
-  // })
-  const data = await resolveAfter2Seconds({
-    username, 
-    password
-  }) as User;
+  const response = await fetch(`${process.env.API_URL}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
   // Transform or forward the response 
-  // const data = await response.json();
-
-
+  const data = await response.json();
   const transformed = { 
     ...data, 
     params : {
