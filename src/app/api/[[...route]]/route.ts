1e75890d9e771c34a16f70cf54e4747831138db3
leon <554638047@qq.com>
2025-03-18 16:14:22 +0800

    feat: nodejs

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,7 +1,7 @@
 import { Hono, Context } from 'hono';
 import { handle } from 'hono/vercel';
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 const app = new Hono().basePath('/api')
 
