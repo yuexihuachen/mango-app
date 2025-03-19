7849e62d7a9ea65ffc5ed0758f2f8a714f7faffe
leon <554638047@qq.com>
2025-03-19 15:22:20 +0800

    feat: get sign out

--- src/app/(admin)/signout/page.tsx
+++ src/app/(admin)/signout/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 import { useEffect } from 'react'
 import { signOut } from '@/app/actions'
-import { redirect } from "next/navigation";
  
 export default function Page() {
   useEffect(() => {
     signOut()
   }, [])
 
-  return redirect('/signin');
+  return null;
 }
